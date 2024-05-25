ff9c6525e5f288ccb58db05e828ecf776438fb71
lth-1026 <th20001026@gmail.com>
2024-05-25 02:18:27 +0200

    step3, add fun about number of options and option list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "de.fra_uas.fb2.mobiledevices.bordasvotingmethod"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "de.fra_uas.fb2.mobiledevices.bordasvotingmethod"
@@ -41,7 +41,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/MainActivity.kt
+++ app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/MainActivity.kt
@@ -2,10 +2,72 @@ package de.fra_uas.fb2.mobiledevices.bordasvotingmethod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.core.text.set
+import androidx.core.widget.addTextChangedListener
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var numberOptionEt: EditText
+    private lateinit var votingEt: EditText
+
+    private var optionCount: Int = 3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberOptionEt = findViewById(R.id.numberOptionEt)
+        votingEt = findViewById(R.id.votingEt)
+
+        numberOptionEt.setOnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus) {
+                handleOptionCount()
+            }
+        }
+
+        votingEt.setOnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus) {
+               handleOptions()
+            }
+        }
+    }
+
+    private fun handleOptionCount() {
+        val number = numberOptionEt.text.toString().toInt()
+
+        if(number < 2 || number > 10) {
+            //todo: do something
+        }
+
+        optionCount = number
+
+    }
+
+    private fun handleOptions() {
+        val inputValue = votingEt.text.toString()
+
+        var options = mutableListOf<String>()
+        var optionsSize = 0
+
+        if(inputValue.isNotEmpty()) {
+            options = inputValue.split(",").toMutableList()
+            optionsSize = options.size
+        }
+
+        if(optionsSize < optionCount) {
+            while (optionsSize < optionCount) {
+                options.add("Option x")
+                optionsSize++
+            }
+        } else if(optionsSize > optionCount) {
+            options.subList(optionCount, optionsSize).clear()
+        }
+
+        votingEt.setText(options.joinToString(","))
     }
 }
